06c40f95508dc200c6cbec5c9fe37c72e084ff64
lovemexd <101887701+lovemexd@users.noreply.github.com>
2022-12-23 20:13:20 +0300

    Add files via upload

---
+++ JavaFirstMirea-master/src/ru/mirea/task2/Shape.java
@@ -0,0 +1,12 @@
+package ru.mirea.task2;
+
+public class Shape {
+    double width;
+    double length;
+
+    public String toString() {
+        return "\n[Shape]" +
+                "\nwidth: " + width +
+                "\ncolor: " + length + "\n";
+    }
+}
